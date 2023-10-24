dccced09fa4a64e07fedfc22505ed921faf321a8
PRIYAM266 <priyam266german@gmail.com>
2023-10-24 19:02:51 +0530

    Change contact form end-points with nodemailer

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,63 @@
+"use client";
+
+import { useState } from "react";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !email || !message) {
+      console.log("Please fill in all fields");
+      setSubmitted(true);
+      return;
+    }
+
+    // Check if the email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      console.log("Please enter a valid email address");
+      return;
+    }
+
+    console.log("Sending");
+    let data = {
+      name,
+      email,
+      message,
+    };
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received");
+      if (res.status === 200) {
+        console.log("Response succeeded!");
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSubmitted(false);
+      }
+    });
+  };
+
   return (
     <section
       id={"contact"}
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
       <form
-        method="POST"
-        action="https://getform.io/f/adfc1e41-510a-4372-bd08-37aaeb61e67c"
         className="flex flex-col max-w-[600px] w-full"
+        method="POST"
+        onSubmit={handleSubmit}
       >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
@@ -18,27 +68,47 @@ const Contact = () => {
             <p>priyam.g266@gmail.com</p>
           </div>
         </div>
+
         <input
           className="bg-[#ccd6f6] p-2 text-black"
           type="text"
           placeholder="Name"
           name="name"
-          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
         />
+
         <input
           className="my-4 p-2 bg-[#ccd6f6] text-black"
           type="email"
           placeholder="Email"
           name="email"
-          id="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
         />
+
         <textarea
           className="bg-[#ccd6f6] p-2 text-black"
           name="message"
           rows={10}
           placeholder="Message"
           id="message"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          value={message}
         ></textarea>
+
+        {!submitted ? null : (
+          <p className="text-pink-600">
+            *Please fill all the details correctly.
+          </p>
+        )}
+
         <button
           type="submit"
           className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
---
+++ src/pages/api/contact.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  try {
+    console.log(req.body);
+    let nodemailer = require("nodemailer");
+    const transporter = nodemailer.createTransport({
+      pool: true,
+      port: 587,
+      host: "smtp.office365.com",
+      auth: {
+        user: "priyam.266gupta@outlook.com",
+        pass: process.env.EMAIL_FROM_PASSWORD,
+      },
+      secure: false,
+      maxConnections: 1,
+    });
+    const mailData = {
+      from: "priyam.266gupta@outlook.com",
+      to: process.env.EMAIL_TO,
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`,
+    };
+    transporter.sendMail(mailData, function (err: Error | null, info: any) {
+      if (err) console.log(err);
+      else console.log(info);
+    });
+    return res.status(200).json({ message: "Success" });
+  } catch (error: any) {
+    throw new Error(`Failed to call the api: ${error.message}`);
+  }
+}
